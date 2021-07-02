b3afa9e18e4bc156adb343a1cb3762387abb543f
Mohammad Ghasemi <mohstarsos1@Mohammads-MacBook-Pro.local>
2021-07-02 15:38:00 +0430

    add some

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <Tutorial/>
+  <div
+    class="
+      bg-cover
+      from-gray-900
+      to-green-700
+      text-gray-900
+      bg-gradient-to-r
+      h-screen
+      pt-6
+    "
+  >
+    <Post />
+  </div>
 </template>
 
 <script>
